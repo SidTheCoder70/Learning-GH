954454b8be8de6a4fa8170030e431d2bf6cd09b4
mudguts70 <e_ruston@hotmail.com>
2023-04-12 19:48:03 -0600

    Initial Commit

---
+++ M3L2 LearningGH/M3L2_LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  M3L2_LearningGHApp.swift
+//  M3L2 LearningGH
+//
+//  Created by Eric Ruston on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct M3L2_LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
