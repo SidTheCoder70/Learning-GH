59c11d657c4d7b2640d63786ec129228d8568b39
SidTheCoder70 <130602310+SidTheCoder70@users.noreply.github.com>
2023-04-12 21:57:11 -0600

    Update ContentView to say What's up!

--- M3L2 LearningGH/ContentView.swift
+++ M3L2 LearningGH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, You fucking cocksucker")
+            Text("What's up, Code Crew")
         }
         .padding()
     }
